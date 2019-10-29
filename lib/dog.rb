a6b6e59f4494b3d3938dd1cfa43e836e8f9699d0
Michael Waldo <mbwcogic@yahoo.com>
2019-10-29 17:32:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,11 +3,11 @@ def initialize(name)
     @name = name
   end
  
-  def breed=(breed)
-    @breed = breed
-  end
+  #def breed=(breed)
+   # @breed = breed
+  #end
  
-  def breed
-    @breed
-  end
+  #def breed
+   # @breed
+  #end
 end
